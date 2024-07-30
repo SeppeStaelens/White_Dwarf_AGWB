8da17c12a15eab8199596851b8ebba70a853369a
Seppe Staelens <seppe.staelens@hotmail.com>
2024-07-30 17:12:53 +0100

    first release?

--- doc/html/search/all_9.js
+++ doc/html/search/all_9.js
@@ -4,12 +4,13 @@ var searchData=
   ['make_5fomega_5fplot_5funnorm_1',['make_Omega_plot_unnorm',['../namespacemodules_1_1auxiliary.html#ad2f66663ad7443ea14bd8ceb9748ee3d',1,'modules::auxiliary']]],
   ['max_5fz_2',['max_z',['../classmodules_1_1SimModel_1_1SimModel.html#a13e035a8305bfd8b92b19003985164a9',1,'modules::SimModel::SimModel']]],
   ['modules_3',['modules',['../namespacemodules.html',1,'']]],
-  ['modules_3a_3aadd_5fbirth_4',['add_birth',['../namespacemodules_1_1add__birth.html',1,'modules']]],
-  ['modules_3a_3aadd_5fbulk_5',['add_bulk',['../namespacemodules_1_1add__bulk.html',1,'modules']]],
-  ['modules_3a_3aadd_5fmerge_6',['add_merge',['../namespacemodules_1_1add__merge.html',1,'modules']]],
-  ['modules_3a_3aauxiliary_7',['auxiliary',['../namespacemodules_1_1auxiliary.html',1,'modules']]],
-  ['modules_3a_3aphysics_8',['physics',['../namespacemodules_1_1physics.html',1,'modules']]],
-  ['modules_3a_3aredshiftinterpolator_9',['RedshiftInterpolator',['../namespacemodules_1_1RedshiftInterpolator.html',1,'modules']]],
-  ['modules_3a_3asfh_10',['SFH',['../namespacemodules_1_1SFH.html',1,'modules']]],
-  ['modules_3a_3asimmodel_11',['SimModel',['../namespacemodules_1_1SimModel.html',1,'modules']]]
+  ['modules_4',['Modules',['../namespacemodules.html#autotoc_md0',1,'']]],
+  ['modules_3a_3aadd_5fbirth_5',['add_birth',['../namespacemodules_1_1add__birth.html',1,'modules']]],
+  ['modules_3a_3aadd_5fbulk_6',['add_bulk',['../namespacemodules_1_1add__bulk.html',1,'modules']]],
+  ['modules_3a_3aadd_5fmerge_7',['add_merge',['../namespacemodules_1_1add__merge.html',1,'modules']]],
+  ['modules_3a_3aauxiliary_8',['auxiliary',['../namespacemodules_1_1auxiliary.html',1,'modules']]],
+  ['modules_3a_3aphysics_9',['physics',['../namespacemodules_1_1physics.html',1,'modules']]],
+  ['modules_3a_3aredshiftinterpolator_10',['RedshiftInterpolator',['../namespacemodules_1_1RedshiftInterpolator.html',1,'modules']]],
+  ['modules_3a_3asfh_11',['SFH',['../namespacemodules_1_1SFH.html',1,'modules']]],
+  ['modules_3a_3asimmodel_12',['SimModel',['../namespacemodules_1_1SimModel.html',1,'modules']]]
 ];
